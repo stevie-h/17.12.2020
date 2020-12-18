3254125eb2eef89a3d3a9be85cbdcbeec4294285
Stevie H <74920914+stevie-h@users.noreply.github.com>
2020-12-18 17:57:23 +0200

    Update hw17122020stevieh.java

--- hw17122020stevieh.java
+++ hw17122020stevieh.java
@@ -1,9 +1,12 @@
-/*
-
-
-
-
+/* HW 17/12/2020
+1. write for-loop which prints the numbers from 100 to 1000 in 100 steps: 100, 200, 300, .... 1000
+2. write for-loop which prints numbers from 5 to -10 (minus)
+3. *etar: use for-loop to print the power of 2: 1, 2, 4, 8, 16, 32, 64, ... 2048
+4. generate a random number from 1-3
+   use switch-case to print the number in hebrew.
+   for exmaple if 1 was generated -> print "ehad", 2 -> print "shtaim" , 3 -> print "shalosh"
 */
+
 
 package com.company;
 
