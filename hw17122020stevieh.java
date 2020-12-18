adcf0bd214c8216a67bf797853e596d77ea5ef92
Stevie H <74920914+stevie-h@users.noreply.github.com>
2020-12-18 17:55:40 +0200

    Create hw17122020stevieh.java

---
+++ hw17122020stevieh.java
@@ -0,0 +1,19 @@
+/*
+
+
+
+
+*/
+
+package com.company;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+      
+      
+      
